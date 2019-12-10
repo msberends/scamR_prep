eae4be064c50af0254a696949349b22ec14f216f
Johannes Dreesman <johannesdreesman@yahoo.de>
2019-12-10 14:37:22 +0100

    Adding convec hull and labels

--- R/plotClarity.R
+++ R/plotClarity.R
@@ -1,9 +1,21 @@
 plotClarity <- function(clarity) {
   arg_clarity <- clarity
+  diamconvhull <- diamonds %>% 
+    filter(clarity == arg_clarity) %>% 
+    select(carat, price) %>% 
+    chull()
+  diamconvhull2 <- diamonds %>% 
+    filter(clarity == arg_clarity) %>% 
+    select(carat, price) %>% 
+    slice(diamconvhull)
+  
   ggplot() +
     geom_point(data = diamonds,
-               aes(price, carat)) +
+               aes(carat, price)) +
     geom_point(data = diamonds %>% 
                  filter(clarity == arg_clarity),
-               aes(carat, price), colour = "red")
+               aes(carat, price), colour = "red") +
+    geom_polygon(data=diamconvhull2, 
+                 aes(carat, price), colour = "red", fill="red", alpha=0.5) +
+    labs(title="Diamonds") + xlab("Weight in carat") + ylab("Price in dollar")
 }
